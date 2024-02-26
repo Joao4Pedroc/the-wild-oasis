94f8e5799744f6a7c9805a030a50a186d50b126c
TratosXimbriba <soprazua123lol@gmail.com>
2024-02-26 15:28:08 -0300

    Feat: Reusable table

--- src/features/cabins/useCabins.js
+++ src/features/cabins/useCabins.js
@@ -4,8 +4,8 @@ import { getCabins } from "../../services/apiCabins";
 export function useCabins() {
   const {
     isLoading,
-    data: cabin,
+    data: cabins,
     error,
-  } = useQuery({ queryKey: ["cabin"], queryFn: getCabins });
-  return { isLoading, error, cabin };
+  } = useQuery({ queryKey: ["cabins"], queryFn: getCabins });
+  return { isLoading, error, cabins };
 }
