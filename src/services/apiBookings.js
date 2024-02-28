3048415713dca52930a80cb7c5708dde7053ec58
TratosXimbriba <soprazua123lol@gmail.com>
2024-02-28 15:43:22 -0300

    Feat: bookings sort

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -11,7 +11,13 @@ export async function getBookings({ filter, sortBy }) {
     );
 
   // filter
-  if (filter !== null) query = query.eq(filter.field, filter.value);
+  if (filter) query = query.eq(filter.field, filter.value);
+
+  // sort
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
 
   const { data, error } = await query;
 
