91296fc3ce1cf8eab385b0a1ef6da143e5e3ca55
TratosXimbriba <soprazua123lol@gmail.com>
2024-03-02 19:30:38 -0300

    Feat: login

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
+
+import { useLogin } from "./useLogin";
+
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
+import SpinnerMini from "../../ui/SpinnerMini";
 import FormRowVertical from "../../ui/FormRowVertical";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("tratos@ximbriba.com");
+  const [password, setPassword] = useState("tratosximbriba");
+  const { login, isLoading: isLogging } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -20,8 +30,10 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLogging}
         />
       </FormRowVertical>
+
       <FormRowVertical label="Password">
         <Input
           type="password"
@@ -29,10 +41,13 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLogging}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLogging}>
+          {!isLogging ? "Log ion" : <SpinnerMini />}
+        </Button>
       </FormRowVertical>
     </Form>
   );
